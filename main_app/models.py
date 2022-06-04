1cf14bc88d72632f1d4425fe9e9c8f580b104f9b
Maria Jimenez <mtjimenez@gmail.com>
2022-06-04 16:17:32 -0400

    User Model given Group FK and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
-class User(models.Model):
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    goal_description = models.CharField(max_length=500)
-    avatar_img = models.CharField(max_length=500)
-  
-    
-    def __str__(self):
-        return self.first_name
-
-    class Meta:
-        ordering = ['first_name']
 
 
 class Group(models.Model):
@@ -29,5 +15,24 @@ class Group(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+
+class User(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    goal_description = models.CharField(max_length=500)
+    avatar_img = models.CharField(max_length=500)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="users", default='1')
+  
+    
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
 
 
