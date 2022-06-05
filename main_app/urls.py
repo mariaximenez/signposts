d4fe5b007536983a6b0418d64dde951cb5f0e417
Maria Jimenez <mtjimenez@gmail.com>
2022-06-05 11:50:40 -0400

    Detail Page with Update and Delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.Login.as_view(), name="login"),
     path('users/', views.UserList.as_view(), name="user_list"), 
-    path('groups/', views.GroupList.as_view(), name="group_list"), 
+    path('groups/', views.GroupList.as_view(), name="group_list"),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name="user_detail"),
+    path('groups/<int:pk>/', views.GroupDetail.as_view(), name="group_detail"),
+    path('users/<int:pk>/update/', views.UserUpdate.as_view(), name="user_update"),
+    path('users/<int:pk>/delete/', views.UserDelete.as_view(), name="user_delete"),
+    path('groups/<int:pk>/update/', views.GroupUpdate.as_view(), name="group_update"),
+    path('groups/<int:pk>/delete/', views.GroupDelete.as_view(), name="group_delete"),
+
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import User as UserModel
 from .models import Group as GroupModel
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
 
 
@@ -38,7 +41,45 @@ class GroupList(TemplateView):
         return context
 
 
+class GroupDetail(DetailView):
+    model = GroupModel
+    template_name = "group_detail.html"
 
+
+class UserDetail(DetailView):
+    model = UserModel
+    template_name = "user_detail.html"
+
+
+class GroupUpdate(UpdateView):
+    model = GroupModel
+    fields = ['name', 'goal', 'goal_img']
+    template_name = "group_update.html"
+
+
+    def get_success_url(self):
+        return reverse('group_detail', kwargs={'pk': self.object.pk})
+ 
+
+class GroupDelete(DeleteView):
+    model = GroupModel
+    template_name = "group_delete_confirmation.html"
+    success_url = "/groups/"
+
+class UserUpdate(UpdateView):
+    model = UserModel
+    fields = ['first_name', 'last_name', 'username', 'password', 'goal_description','avatar_img', 'group']
+    template_name = "user_update.html"
+
+
+    def get_success_url(self):
+        return reverse('user_detail', kwargs={'pk': self.object.pk})
+ 
+
+class UserDelete(DeleteView):
+    model = UserModel
+    template_name = "user_delete_confirmation.html"
+    success_url = "/users/"
 
 class Post(TemplateView):
      template_name = "posts.html"
