01771faff7959d57e55845a3544993920859cd1c
Maria Jimenez <mtjimenez@gmail.com>
2022-06-12 20:12:16 -0400

    Resizing hero class

--- db/create_comments.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO main_app_comment(text, date)
-    ('I love this post', '2021-05-07 18:57:42.517205-07');
--- db/create_posts.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO main_app_post(text, date)
-    ('I organized a protest', '2021-05-07 18:57:42.517205-07');
